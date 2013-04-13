72823c079dc4120f572b87546aacc7b6d43d63a8
Hurricane Hamilton <hurricane@alpha.(none)>
2013-04-13 13:23:52 -0400

    skeleton do baleing

--- src/TheBoss.java
+++ src/TheBoss.java
@@ -1,9 +1,11 @@
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import bonzai.api.Entity;
 import bonzai.api.Farmhand;
 import bonzai.api.GameState;
+import bonzai.api.Item;
+import bonzai.api.Tile;
 import bonzai.api.list.FarmhandList;
 
 
@@ -11,7 +13,7 @@ public class TheBoss {
 	
 	public TheBoss(GameState state){
 		this.states = new HashMap<Integer,STATES>();
-		this.pathfinder = new Pathfinder();
+		this.pathfinder = new AStarPathfinder(state);
 		for (Integer i = 0;i<state.getMyFarmhands().size();++i)
 			states.put(i, STATES.NOSTATE);		
 	}//end constructor
@@ -29,12 +31,41 @@ public class TheBoss {
 		
 	}
 	
-	private void doBaleing(){
+	private void doBaleing(Integer i, GameState gs){
+		Tile myBase = gs.getMyBase();
+		Farmhand fh = gs.getFarmhands().get(i);
+		Entity holding = fh.getHeldObject();
+		Item item = null;
+		if(holding instanceof Item){
+			item = (Item) holding;
+		}
+		// do I have a pitchfork?
+		if(item != null && item.getType() == Item.Type.Pitchfork){
+			// do I have a bale?
+			if(item.getFull()){
+				
+			}
+		}else{
+			//Am I at the base?
+			if(isAdjacent(myBase.getX(), myBase.getY(), fh.getX(), fh.getY())){
+				//Am I holding something? Try to sell it
+			}
+		}
+			
+			
 		
 	}
 	
 	private void doTaunting(){
 		
+	}
+	
+	private void doRetreat(){
+		
+	}
+	
+	public boolean isAdjacent(int x1, int y1, int x2, int y2){
+		return Math.abs(x1-x2) <=1 && Math.abs(y1-y2) <=1;
 	}
 	
 	private enum STATES{
