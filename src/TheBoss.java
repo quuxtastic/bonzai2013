57953e025525c1c7fbc2ee3d28bd2c0d4090ab2b
Michael Godfrey <mjgodfre@mtu.edu>
2013-04-13 12:34:00 -0400

    Adding Explitives

--- src/TheBoss.java
+++ src/TheBoss.java
@@ -3,23 +3,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 import bonzai.api.Farmhand;
+import bonzai.api.GameState;
 import bonzai.api.list.FarmhandList;
 
 
 public class TheBoss {
 	
-	public TheBoss(FarmhandList farmhandList){
-		this.farmhandList  = farmhandList;
+	public TheBoss(GameState state){
 		this.states = new HashMap<Integer,STATES>();
 		this.pathfinder = new Pathfinder();
-		for (Integer i = 0;i<this.farmhandList.size();++i)
+		for (Integer i = 0;i<state.getMyFarmhands().size();++i)
 			states.put(i, STATES.NOSTATE);		
 	}//end constructor
 	
-	public void getNext(){
+	public void process(GameState state){
+		FarmhandList hands = state.getMyFarmhands();
 		
 	}
-
+	
+	private void doDucking(){
+		
+	}
+	
+	private void doEgging(){
+		
+	}
+	
+	private void doBaleing(){
+		
+	}
+	
+	private void doTaunting(){
+		
+	}
+	
 	private enum STATES{
 		NOSTATE,
 		DUCKING,
@@ -29,7 +46,6 @@ public class TheBoss {
 		SELLING
 		}
 	
-	private FarmhandList farmhandList;
 	private Map<Integer,STATES> states;
 	private Pathfinder pathfinder;
 }
